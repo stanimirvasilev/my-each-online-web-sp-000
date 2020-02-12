a0ff46e7cb4cef9a9069157706871264bc7a39cb
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-02-12 11:02:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,10 @@
 def my_each(words) # put argument(s) here
 i = 0
 while i < words.length 
- i = i + 1 
-yield([0])
+
+yield words[0]
+i = i + 1 
   # code here
 end
 end
  
-
- my_each(words) do |word|
-   return word
- end
