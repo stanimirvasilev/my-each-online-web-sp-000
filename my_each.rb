edba76b8d44aa77148c5bd886989e88ab459abec
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-02-12 10:47:21 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,12 @@
 def my_each(words) # put argument(s) here
-while
+i = 0
+while i < array.length 
+ i = i + 1 
 yield
   # code here
 end
 end
  
+ yielding_over_words { |word| 
+   return word 
+ }
